4c0aad2465ca0a96b6a64ab13b432881a05bcf2f
MASATO FUJIWARA <masatonww0312@gmail.com>
2020-06-22 13:58:02 +0900

    当たり判定を変更追加しました

--- α版制作/αMETALSLUG/collision.h
+++ α版制作/αMETALSLUG/collision.h
@@ -47,6 +47,17 @@ public:
 		OBJTYPE_MAX
 	} OBJTYPE;
 
+	// 当たり判定の主な種類
+	typedef enum
+	{
+		COLLISIONTYPE_NORMAL = 0,			// 中心点を原点にする判定
+		COLLISIONTYPE_CHARACTER,			// 足元を原点にする判定
+		COLLISIONTYPE_OTHER,				// 中心点を原点とするオブジェクトと足元を原点にするオブジェクトで判定
+		COLLISIONTYPE_MAX
+	} COLLISIONTYPE;
+
+
+
 	/* メンバ関数 */
 	CCollision();
 	~CCollision();
@@ -55,37 +66,36 @@ public:
 	void Update(void);
 
 	/* 静的メンバ関数 */
-	static void UpdateAll(void);				// 当たり判定更新
-	static void Delete(CCollision *pColl);		// 当たり判定削除
-	static void DeleteAll(void);				// 全ての当たり判定削除
-	static CCollision * Create(void);			// 当たり判定の生成
+	static void ReleaseCollision(CCollision *pColl);	// 当たり判定削除
+	static CCollision * Create(void);					// 当たり判定の生成
 
 	/* メンバ関数 */
-	void SetPos(D3DXVECTOR3 *pos);				// 座標の設定
-	void SetSize(D3DXVECTOR3 size);				// サイズの設定
-	void SetSize2D(D3DXVECTOR3 size);			// 2D用サイズの設定
-	void SetMove(D3DXVECTOR3 *move);			// 移動量の設定
-	void SetType(OBJTYPE type);					// タイプの設定
-	bool Collision(OBJTYPE objtype);			// 箱型の当たり判定
-	bool Collision2D(CCollision *pCollision);	// 板型の当たり判定
-	bool GetHitFlag() {return m_bHitFlag;}		// 判定の結果を返す
-	OBJTYPE GetObjtype() {return m_objtype;};	// 誰の当たり判定なのかを返す
+	void SetPos(D3DXVECTOR3 *pos);						// 座標の設定
+	void SetPosOld(D3DXVECTOR3 *posold);				// 1フレーム前の座標の設定
+	void SetSize(D3DXVECTOR3 size);						// サイズの設定
+	void SetSize2D(D3DXVECTOR3 size);					// 2D用サイズの設定
+	void SetMove(D3DXVECTOR3 *move);					// 移動量の設定
+	void SetType(OBJTYPE type);							// タイプの設定
+	bool Collision(OBJTYPE objtype);					// 箱型の当たり判定
+	bool Collision2D(CCollision *pCollision);			// 板型の当たり判定
+	bool CharCollision2D(CCollision *pCollision);		// キャラクター用板型の当たり判定
+	bool OtherCollision2D(CCollision *pCollision);			// 板型の当たり判定
+	OBJTYPE GetObjtype() {return m_objtype;};			// 誰の当たり判定なのかを返す
+	void DeCollisionCreate(COLLISIONTYPE collisiontype);// デバッグ用当たり判定の生成
 
 protected:
 private:
 	/* メンバ変数 */
-	OBJTYPE				m_objtype;				// タイプ
-	D3DXVECTOR3			*m_ppos;				// 位置情報のポインタ
-	D3DXVECTOR3			m_posOld;				// 前回の位置情報
-	D3DXVECTOR3			m_size;					// サイズ情報
-	D3DXVECTOR3			*m_pmove;				// 移動情報のポインタ
-	static CCollision	*m_pTop;				// 先頭オブジェクトへのポインタ
-	static CCollision	*m_pCur;				// 現在オブジェクトへのポインタ
-	CCollision			*m_pNext;				// 次のオブジェクトへのポインタ
-	CCollision			*m_pPrey;				// 前のオブジェクトへのポインタ
-	CDebugcollision		*m_Debugcollision;		// デバッグ用当たり判定のポインタ
-	bool				m_bDeadFlag;			// 死亡フラグ
-	bool				m_bHitFlag;				// 当たり判定に触れかどうか
+	OBJTYPE				m_objtype;						// タイプ
+	COLLISIONTYPE		m_Collisiontype;				// 当たり判定のタイプ
+	D3DXVECTOR3			*m_ppos;						// 位置情報のポインタ
+	D3DXVECTOR3			*m_posOld;						// 前回の位置情報
+	D3DXVECTOR3			m_size;							// サイズ情報
+	D3DXVECTOR3			*m_pmove;						// 移動情報のポインタ
+	CDebugcollision		*m_Debugcollision;				// デバッグ用当たり判定のポインタ
+	bool				m_bDeadFlag;					// 死亡フラグ
+	bool				m_bHitFlag;						// 当たり判定に触れかどうか
+	CCollision			*m_pCollision;					// 当たり判定のポインタ
 };
 
 #endif
