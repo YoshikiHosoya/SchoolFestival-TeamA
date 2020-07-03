9c6452f8ac02c95422da984064c336386dd60853
YUTA SAKAI <yutiimaru0209@gmail.com>
2020-07-03 17:11:52 +0900

    マップ変更とモーションの追加

--- α版制作/αMETALSLUG/SourceCode/Character.h
+++ α版制作/αMETALSLUG/SourceCode/Character.h
@@ -74,6 +74,10 @@ public:
 		PLAYER_MOTION_GRENADE,				//グレネード
 		PLAYER_MOTION_JUMPSHOOT,			//ジャンプ撃ち
 		PLAYER_MOTION_JUMP,					//ジャンプ
+		PLAYER_MOTION_SHOOT,				//撃つ
+		PLAYER_MOTION_SQUAT,				//しゃがむ
+		PLAYER_MOTION_SQUATSHOOT,			//しゃがみ撃ち
+
 
 		ENEMY_MOTION_NORMAL,				//ニュートラル
 		ENEMY_MOTION_WALK,					//歩き
