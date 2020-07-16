e1bf3777f53c697c60a6cb919bd13d3edd6d3148
YOSHIKI HOSOYA <yoshiki0123hosoya@gmail.com>
2020-07-16 11:38:55 +0900

    Debug用のグラフ追加　Debug_Collisionのメモリ開放処理追加 パーティクルエディタ改良　

--- β版制作/チーム制作β版/SourceCode/hosso/Debug_WorldLine.cpp
+++ β版制作/チーム制作β版/SourceCode/hosso/Debug_WorldLine.cpp
@@ -1,10 +1,9 @@
 //-----------------------------------------------------------------------------
 //
-//テクスチャ管理  [texture.cpp]
+//基準の線の処理  [Debug_WorldLine.cpp]
 // Author : Yoshiki Hosoya
 //
 //-----------------------------------------------------------------------------
-#include "../texture.h"
 #include "../manager.h"
 #include "../renderer.h"
 #include "../3Dline.h"
@@ -39,9 +38,9 @@ CDebug_WorldLine *CDebug_WorldLine::Create(D3DXVECTOR3 &pos)
 	{
 		pWorldLine->m_Pos = pos;
 
-		pWorldLine->m_pLine[AXIS_X] = C3DLine::Create(pos, ZeroVector3, AXIS_X_OFFSET, -AXIS_X_OFFSET, WhiteColor);
-		pWorldLine->m_pLine[AXIS_Y] = C3DLine::Create(pos, ZeroVector3, AXIS_Y_OFFSET, -AXIS_Y_OFFSET, WhiteColor);
-		pWorldLine->m_pLine[AXIS_Z] = C3DLine::Create(pos, ZeroVector3, AXIS_Z_OFFSET, -AXIS_Z_OFFSET, WhiteColor);
+		pWorldLine->m_pLine[AXIS_X] = C3DLine::Create(pos, ZeroVector3, AXIS_X_OFFSET, -AXIS_X_OFFSET, RedColor);
+		pWorldLine->m_pLine[AXIS_Y] = C3DLine::Create(pos, ZeroVector3, AXIS_Y_OFFSET, -AXIS_Y_OFFSET, GreenColor);
+		pWorldLine->m_pLine[AXIS_Z] = C3DLine::Create(pos, ZeroVector3, AXIS_Z_OFFSET, -AXIS_Z_OFFSET, BlueColor);
 
 		pWorldLine->SetPosOrigin(pos);
 
