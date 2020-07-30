ab3ce40763a2840d57e3ddc379b6b54f0534467c
NORIYUKI HANZAWA <ietgori555@gmail.com>
2020-07-30 15:14:11 +0900

    ライブラリ　スタートボタン追加　ボスの原点変更

--- β版制作/チーム制作β版/SourceCode/hosso/HossoLibrary.h
+++ β版制作/チーム制作β版/SourceCode/hosso/HossoLibrary.h
@@ -116,7 +116,7 @@ public:
 	static void CalcRotation(float &fRot);																		//‰ñ“]‚ð360“xˆÈ“à‚É‚·‚éŒvŽZ
 
 	static bool PressAnyButton(void);
-
+	static bool PressStartButton(void);
 	static bool ImGui_Combobox(std::vector<std::string> aItemNameList, std::string aTitle, int &nValue);
 
 	//------------------------------------------------------------------------------
